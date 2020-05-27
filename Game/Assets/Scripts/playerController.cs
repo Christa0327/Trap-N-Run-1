c10779976327f1287f0b4f1f18f68d7a69cf6f4f
hcw8250 <62008194+hcw8250@users.noreply.github.com>
2020-05-27 14:54:59 +1200

    Game Files

---
+++ Game/Assets/Scripts/playerController.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    public KeyCode left;
+    public KeyCode right;
+    public KeyCode jump;
+
+
+    private Rigidbody2D theRB;
+    
+
+    public Transform groundCheckPoint;
+    public bool isGrounded;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public Vector3 respawnPoint; //Create spawn point 
+
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theRB = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Spike")  //IF player interacts with Spike, changep position to repsawn point
+        {
+            transform.position = respawnPoint;
+        }
+
+        if (other.tag == "Respawn")
+        {
+            respawnPoint = other.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+
+        if (Input.GetKey(left))
+        {
+            theRB.velocity = new Vector2(-moveSpeed, theRB.velocity.y);
+        }
+        else if (Input.GetKey(right))
+        {
+            theRB.velocity = new Vector2(moveSpeed, theRB.velocity.y);
+        }
+        else
+        {
+            theRB.velocity = new Vector2(0, theRB.velocity.y);
+        }
+
+        if (Input.GetKeyDown(jump) && isGrounded)
+        {
+            theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
+        }
+
+        if (theRB.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+        }
+        else if (theRB.velocity.x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+        }
+
+        anim.SetFloat("Speed", Mathf.Abs(theRB.velocity.x));
+        anim.SetBool("Grounded", isGrounded);
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Trap-N-Run
+Development of a two player trap and running game
